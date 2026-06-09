132b0f552a6d49b57511892074f5024844cb7c31
kinachouhan <chouhankina@gmail.com>
2026-06-09 18:55:42 +0530

    Add inventory management functions including product addition, removal, stock update, and purchase tracking

--- index.js
+++ index.js
@@ -109,18 +109,298 @@
 // console.log(getCartTotal(cart))
 
 // 10. Student Average Generator
-let students= [
-{ name:"Ritik", marks: [80,90,85] },
-{ name:"Aman", marks: [70,75,80] }
-]
-function getAverageMarks(students){
-   students.map( (student)=>{
-        let total = student.marks.reduce( (acc, val)=>{
-            return acc+val
-        }, 0)
-        let average = total/ student.marks.length
-        console.log(student.name , average)
+// let students= [
+// { name:"Ritik", marks: [80,90,85] },
+// { name:"Aman", marks: [70,75,80] }
+// ]
+// function getAverageMarks(students){
+//    students.map( (student)=>{
+//         let total = student.marks.reduce( (acc, val)=>{
+//             return acc+val
+//         }, 0)
+//         let average = total/ student.marks.length
+//         console.log(student.name , average)
+//     })
+// }
+// getAverageMarks(students)
+
+// // 11. Inventory Search
+// let
+// inventory= [
+// { id:1, name:"Laptop" },
+// { id:2, name:"Mouse" },
+// { id:3, name:"Monitor" }
+// ]
+// function findProductById(id){
+//     return inventory.filter( item => item.id == id)
+// }
+// console.log(findProductById(1))
+
+
+// // 12. Usernames generators
+// let users= [
+// { name:"Ritik Rajput" },
+// { name:"Aman Gupta" }
+// ]
+
+// function userNameGenerator(users){
+//     return users.map( user => user.name.split(" ").join("_"))
+// }
+// console.log(userNameGenerator(users))
+
+// // 13. Highest Scoring Student
+// let students = [
+//     { name: "Ritik", marks: 85 },
+//     { name: "Aman", marks: 95 },
+//     { name: "Priya", marks: 75 }
+// ]
+
+// function findHighestScore(students) {
+//     let highestScoringStudent
+//     let highestScore = 0
+//     students.map((student) => {
+//         if (student.marks > highestScore) {
+//             highestScore = student.marks
+//             highestScoringStudent = student.name
+//         }
+//     })
+//     console.log(highestScoringStudent)
+// }
+// findHighestScore(students)
+
+// // 14. Update product stock 
+// let products = [
+//     { id:1 , name: "Mouse", price: 500, qty: 2 },
+//     { id:2 , name: "Keyboard", price: 1000, qty: 1 },
+//     { id:3 , name: "Monitor", price: 10000, qty: 1 }
+// ]
+
+// function updateStock(id, quantity){
+//     let product = products.find( product => product.id == id)
+//     if(product){
+//         product.qty = quantity
+//     }
+//     return products
+// }
+// console.log(updateStock(2 , 1))
+
+// // 15. Sort Students by Marks
+// let data = [
+// {name:"A", marks:70},
+// {name:"B", marks:95},
+// {name:"C", marks:80}
+// ]
+
+// function sortStudents(data){
+//     return data.sort( (a ,b)=> a.marks - b.marks)
+// }
+// console.log(sortStudents(data))
+
+// 16. Student grade report
+// let students = [
+//     { name: "Ritik", marks: [80, 90, 85] },
+//     { name: "Aman", marks: [50, 40, 60] }
+// ]
+
+// function generateReport(students) {
+//     let result = []
+//     students.map((student) => {
+//         let total = student.marks.reduce((acc, val) => {
+//             return acc + val
+//         }, 0)
+//         let average = total / student.marks.length
+//         let grade
+//         if (average > 80) {
+//             grade = "A"
+//         } else if (average > 60) {
+//             grade = "B"
+//         } else if (average > 40) {
+//             grade = "C"
+//         } else {
+//             grade = "D"
+//         }
+//         result.push({
+//             name: student.name,
+//             average,
+//             grade
+//         })
+//     })
+//     return result
+// }
+// console.log(generateReport(students))
+
+// // 17. Product Revenue Analyzer
+// let products = [
+//     { name: "Mouse", price: 500, sold: 20 },
+//     { name: "Keyboard", price: 1000, sold: 10 }
+// ]
+
+// function revenueAnalyzer(products) {
+//     let revenuePerProduct = products.map((product) => ({
+//         name: product.name,
+//         revenue: product.price * product.sold
+//     }))
+
+//     let totalrevenue = revenuePerProduct.reduce((acc, val) => acc + val.revenue, 0)
+
+//     let bestSeller = products.reduce((best, product) => {
+//         return product.sold > best.sold ? product : best
+//     })
+//     return { revenuePerProduct, totalrevenue, bestSeller }
+// }
+
+// revenueAnalyzer(products)
+
+// 18. Attendence system
+// let
+// students= [
+// { name:"Ritik", present:true },
+// { name:"Aman", present:false },
+// { name:"Priya", present:true }
+// ]
+
+// function countPresent(students){
+//      let count = 0
+//      students.map( (student) =>{
+//          if(student.present == true){
+//             count++
+//          }
+//      })
+//      return count
+// }
+// console.log(countPresent(students))
+
+// function countAbsent(stuldents){
+//      let count = 0
+//      students.map( (student)=>{
+//         if(student.present == false){
+//             count++
+//         }
+//      })
+//      return count
+// }
+// console.log(countAbsent(students))
+
+// function getPresentStudents(students){
+//     return students.filter( student => student.present == true)
+// }
+// console.log(getPresentStudents(students))
+
+// 19. Library Management system
+// let data = [{
+// id:1,
+// title:"Atomic Habits",
+// borrowed:false
+// }]
+
+// function addBook(bookName){
+//     data.push({
+//         id: data.length+1,
+//         title: bookName,
+//         borrowed: false
+//     })
+// }
+// addBook("Love story")
+
+// function borrowBook(id){
+//     let book = data.find( book => book.id == id)
+//     if(book){
+//         book.borrowed = true
+//     }
+// }
+// borrowBook(2)
+
+// function returnBook(id){
+//     let book = data.find( book => book.id == id)
+//     if(book){
+//         book.borrowed = false
+//     }
+// }
+// returnBook(2)
+
+// function showAvailableBooks(){
+//     return data.filter( book => book.borrowed == false)
+// }
+// console.log(showAvailableBooks())
+
+
+//20. Order Management system
+// let orders = [{
+// id:1,
+// customer:"Ritik",
+// amount:5000,
+// status:"Pending"
+// }]
+
+// function createOrder(customerName , amount){
+//      orders.push({
+//          id: orders.length+1,
+//          customer: customerName,
+//          amount,
+//          status: "Pending"
+//      })
+// }
+// createOrder("Kina" , 1000)
+
+// function updateStatus(id , status){
+//     let order =  orders.find( order => order.id == id)
+//     if(order){
+//         order.status = status
+//     }
+// }
+// updateStatus(1 , "Delivered")
+
+// function getPendingOrders(){
+//     return orders.filter( order => order.status == "Pending")
+// }
+// console.log(getPendingOrders())
+
+// function getCompletedOrders(){
+//      return orders.filter( order => order.status == "Delivered")
+// }
+// console.log(getCompletedOrders())
+// console.log(orders)
+
+// 21. Mini E-Commerce System
+let products = []
+
+function addProduct(productName , price , stock){
+     products.push({
+        id: products.length+1,
+        name: productName,
+        price,
+        stock
     })
 }
-getAverageMarks(students)
+addProduct("Laptop" , 50000 , 10)
+addProduct("Keyboard" , 1000 , 20)
 
+function removeProduct(id){
+    products = products.filter( (product)=> product.id !== id)
+}
+removeProduct(1)
+
+function updateStock(id , stock){
+     let product = products.find( product => product.id == id)
+     if(product){
+        product.stock = stock
+     }
+}
+updateStock(2, 10)
+
+function purchaseProducts(id){
+    let product = products.find( (product)=> product.id == id)
+    if(product){
+        product.stock-= 1
+    }
+    return `1 ${product.name} purchased`;
+}
+purchaseProducts(2)
+
+function calculateInventoryValues(){
+    return products.reduce( (acc, val)=>{
+        return acc +(val.price*val.stock)
+    },0)
+}
+console.log(calculateInventoryValues())
+console.log(products)
